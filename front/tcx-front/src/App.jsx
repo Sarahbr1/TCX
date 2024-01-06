72bc943c35cd9abde65b02f401a7741d2238e3d3
unknown <besma.hamzaoui1@gmail.com>
2024-01-06 02:00:58 +0100

    added minisidebar

--- front/tcx-front/src/App.jsx
+++ front/tcx-front/src/App.jsx
@@ -1,46 +1,34 @@
 import { useState } from 'react'
 import './App.css'
-import SideBar from './components/SideBar'
+import MiniSideBar from './components/MiniSideBar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    
     <BrowserRouter>
-        <div className='flex'> 
+      <div className='flex'> 
     <div className=' col-auto'>
-    <SideBar />
-    </div>
-    <div>
+      <MiniSideBar></MiniSideBar>
+      </div>
       <Routes>
-        <Route path='/' element={<Acceuil />}></Route>
-        <Route path='/patients' element={<Patients />}></Route>
-        <Route path='/calendrier' element={<Calendrier />}></Route>
-        <Route path='/notifications' element={<Notifications />}></Route>
-        <Route path='/monprofil' element={<Monprofil />}></Route>
+        <Route path='/notifedit' element={<NotifEdit />}></Route>
+        <Route path='/aide' element={<Aide />}></Route>
+
       </Routes>
-    </div>
-   </div>
-   </BrowserRouter>
+      </div>
+    </BrowserRouter>
+    
      
   )
 }
 
 export default App
 
-function Acceuil(){
-  return <h1>welcome home</h1>
-}
-function Patients(){
-  return <h1>Cliquer pour avoir la liste des patients</h1>
-}
-function Calendrier(){
-  return <h1>Rien n'est plannifié pour aujourd'hui</h1>
-}
-function Notifications(){
-  return <h1>Vous n'avez pas de notifications récentes</h1>
-}
-function Monprofil(){
+function NotifEdit(){
   return <h1>john doe</h1>
+}
+function Aide(){
+  return <h1>Comment pourrions nous vous aider</h1>
 }
---
+++ front/tcx-front/src/components/SideBarRouting.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import SideBar from './SideBar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+export default function SideBarRouting() {
+  return (
+    <BrowserRouter>
+        <div className='flex'> 
+    <div className=' col-auto'>
+    <SideBar />
+    </div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Acceuil />}></Route>
+        <Route path='/patients' element={<Patients />}></Route>
+        <Route path='/calendrier' element={<Calendrier />}></Route>
+        <Route path='/notifications' element={<Notifications />}></Route>
+        <Route path='/monprofil' element={<Monprofil />}></Route>
+        <Route path='/parametres' element={<Parametres />}></Route>
+      </Routes>
+    </div>
+   </div>
+   </BrowserRouter>
+  )
+}
+
+function Acceuil(){
+    return <h1>welcome home</h1>
+  }
+  function Patients(){
+    return <h1>Cliquer pour avoir la liste des patients</h1>
+  }
+  function Calendrier(){
+    return <h1>Rien n'est plannifié pour aujourd'hui</h1>
+  }
+  function Notifications(){
+    return <h1>Vous n'avez pas de notifications récentes</h1>
+  }
+  function Monprofil(){
+    return <h1>john doe</h1>
+  }
+  function Parametres(){
+    return <h1>john doe</h1>
+  }
